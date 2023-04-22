735427a4966bc1d47a1cfbe4d0af3bcca571ff45
IagoPorto <iagopm8@gmail.com>
2023-04-22 20:20:46 +0200

    Alguna automatización mas

--- proyecto/compilar.sh
+++ proyecto/compilar.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 gcc consultas.c -o consultas -Wall
 gcc admin.c -o admin -Wall
 gcc reservas.c -o reservas -Wall
 gcc pagos.c -o pagos -Wall
 gcc receptor.c -o receptor -Wall
 gcc anulaciones.c -o anulaciones -Wall
+
+echo "Todo compilado, si hay errores te jodes."
+
+exit
