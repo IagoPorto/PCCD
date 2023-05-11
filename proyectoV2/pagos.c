1ca7890ad36f6dedc74ccd266b35a10e19bee656
LauraBlancoPereiro <laura.blanco.pereiro@gmail.com>
2023-05-11 22:32:11 +0200

    correcion inicializador

--- proyectoV2/pagos.c
+++ proyectoV2/pagos.c
@@ -275,7 +275,7 @@ int main(int argc, char *argv[]){
 
 
    //tiempo que tarda en entrar en la SC en microsegundos,tiempo que tarda en salir desde que sale de SC en microsegundos
-    fprintf (ficheroSalida, "[%i,Pagos,%i,%i]\n", memoria_id ,microsSC,microsSalir);
+    fprintf (ficheroSalida, "[%i,Pagos,%i,%i]\n", mi_id ,microsSC,microsSalir);
 
 
     
