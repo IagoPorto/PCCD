85d3d1df2e38ff757f7be20aa0e6769e72bbd7d2
PedroOR <81985323+PistolitaV2@users.noreply.github.com>
2023-05-12 10:43:41 +0200

    Graficas finais

--- proyectoV2/grafica.py
+++ proyectoV2/grafica.py
@@ -2,13 +2,16 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 
-y =np.loadtxt('Exemplo.txt',skiprows=1,usecols=[1],unpack=True)
-x=range(len(y))
-print(y)
-print(x)
+##z=np.loadtxt('Exemplo.txt',skiprows=1,usecols=[2],unpack=True)
+##y=range(len(z))
+x=[0.0193,0.0252,0.0178,0.0255,88.8325]
+#y=[400,400,400,400,3600]
+y=[400,800,1200,1600,3600]
+
+# print(x)
 
 plt.ioff()
-plt.title('Grafica')
-plt.plot(x,y)
+plt.title('Tiempo de cambio E-L')
+plt.plot(y,x)
 plt.show()
 
